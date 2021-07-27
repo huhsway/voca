bbf1854460148e138e44890adf34f2cf77771182
huhsway <seunghoe9542@naver.com>
2021-07-27 10:02:09 +0900

    useEffect, fetch()로 API 호출

--- src/component/Day.js
+++ src/component/Day.js
@@ -1,4 +1,5 @@
-import dummy from "../db/data.json";
+// import dummy from "../db/data.json";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import Word from "./Word";
 // App.js에서 :day의 값이 useParams 통해서 들어옴
@@ -6,14 +7,26 @@ import Word from "./Word";
 export default function Day() {
 
   const {day} = useParams();
-  const wordList = dummy.words.filter((word) => word.day === Number(day));
+  // const wordList = dummy.words.filter((word) => word.day === Number(day));
+  const [words, setWords] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:3001/words?day=${day}`)
+    .then(res => {
+      return res.json()
+    })
+    .then(data => {
+      setWords(data);
+    })
+  }, [day]);
+  // 의존성 배열에 day를 넣어줘야지 최신 값을 받아올 수 있음
 
   return (
     <>
       <h2>Day {day}</h2>
       <table>
         <tbody>
-          {wordList.map((word) => (
+          {words.map((word) => (
             <Word word = {word} key = {word.id}/>
           ))}
         </tbody>
--- src/component/DayList.js
+++ src/component/DayList.js
@@ -1,11 +1,25 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import dummy from "../db/data.json";
 
 export default function DayList() {
-  console.log(dummy);
+  const [days, setDays] = useState([]);
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    fetch('http://localhost:3001/days')
+    .then(res => {
+      return res.json()
+    })
+    .then(data => {
+      setDays(data);
+    })
+  }, []); // count 배열을 두번째 인자로 넣으면 count가 변경되었을 경우에만 실행된다. 이것을 의존성 배열이라고 함
+  // useEffect에서 렌더링 직후 한번만 실행되게 하려면 두번째 인자에 빈 배열을 넣자
+
   return (
     <ul className="list_day">
-      {dummy.days.map((day) => (
+      {days.map((day) => (
         <li key={day.id}>
           <Link to={`/day/${day.day}`}>Day {day.day}</Link>
         </li>
