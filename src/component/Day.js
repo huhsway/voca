020a4aa88a98eb9a10900969c39d9fca47a43817
huhsway <seunghoe9542@naver.com>
2021-07-27 10:23:55 +0900

    Custom Hooks

--- src/component/Day.js
+++ src/component/Day.js
@@ -1,24 +1,29 @@
 // import dummy from "../db/data.json";
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import Word from "./Word";
 // App.js에서 :day의 값이 useParams 통해서 들어옴
+import useFetch from "../hooks/useFetch";
 
 export default function Day() {
 
   const {day} = useParams();
-  // const wordList = dummy.words.filter((word) => word.day === Number(day));
-  const [words, setWords] = useState([]);
 
-  useEffect(() => {
-    fetch(`http://localhost:3001/words?day=${day}`)
-    .then(res => {
-      return res.json()
-    })
-    .then(data => {
-      setWords(data);
-    })
-  }, [day]);
+  const words = useFetch(`http://localhost:3001/words?day=${day}`)
+
+  // const {day} = useParams();
+  // // const wordList = dummy.words.filter((word) => word.day === Number(day));
+  // const [words, setWords] = useState([]);
+
+  // useEffect(() => {
+  //   fetch(`http://localhost:3001/words?day=${day}`)
+  //   .then(res => {
+  //     return res.json()
+  //   })
+  //   .then(data => {
+  //     setWords(data);
+  //   })
+  // }, [day]);
   // 의존성 배열에 day를 넣어줘야지 최신 값을 받아올 수 있음
 
   return (
